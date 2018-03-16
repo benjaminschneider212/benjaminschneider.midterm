c9d28138dc84f930df3b999cc51fb35c6a7635dc
Benjamin Schneider <benjamin.schneider@wustl.edu>
2018-03-16 17:38:30 -0500

    probability documentation
    made the documentation for probability in the development file

--- devtools.easyRasch/DevelopmentFile.R
+++ devtools.easyRasch/DevelopmentFile.R
@@ -1,3 +1,6 @@
+#This development file contains all of the code and documentation for said code for the package. You will NOT find documentation in the individual R files so that this can be a one stop shop for grading.
+#That being said, the R files contain the necessary Roxygen code in order to generate functioning (hopefully with the correct functionality) functions and helpful help files.
+
 library(devtools)
 library(roxygen2)
 
@@ -8,20 +11,20 @@ load_all(current.code)
 document(current.code)
 
 #code for class structure:
-setClass(Class="Rasch",
+setClass(Class="Rasch", #this is us setting the class. I use upper case R to indicate that this is a class
          representation = representation(
-           name = "character",
-           a = "numeric",
-           y_i = "numeric"
+           name = "character", #this is a character because of course our names will not be numeric
+           a = "numeric", #this will be a vector of n length
+           y_i = "numeric" #this will be a vector of n length
          ),
          prototype = prototype(
-           name = character(),
+           name = character(), #Just doing the same above, I know this does something, but I am not sure what, but I know that is has to be included
            a=numeric(),
            y_i = numeric()
          )
 )
 
-setMethod("initialize", "Rasch", 
+setMethod("initialize", "Rasch", #this sets up our initialize method so that we are able to create objects of the class Rasch
           function(.Object, ...){
             value=callNextMethod()
             return(value)
@@ -29,45 +32,45 @@ setMethod("initialize", "Rasch",
 ) 
 
 #code for Probability function:
-setGeneric(name="probability",
-           def=function(raschObj, theta)
+setGeneric(name="probability", #setting up our generic for the probability function
+           def=function(raschObj, theta) #we have two arguments that can be taken in in this case.
            {standardGeneric("probability")}
 )
 
 setMethod(f="probability",
-          definition=function(raschObj, theta){
-            a<-as.vector(raschObj@a)
-            y_i<-as.vector(raschObj@y_i)
-            n<-as.numeric(length(a))
-            P_ij<-as.vector(NULL)
+          definition=function(raschObj, theta){ #necessary code to set up the function
+            a<-as.vector(raschObj@a) #this subsets the rasch object and extracts a vector for a
+            y_i<-as.vector(raschObj@y_i) #this subsets the rasch object and extracts a vector for y_i
+            n<-as.numeric(length(a)) #this length is used for the for loop below
+            P_ij<-as.vector(NULL) #creation of empty vectors for our eventual outputs
             PQ_ij<-as.vector(NULL)
-            for (i in 1:n){
-              x<-a[i]
-              output<-(exp(theta-x)/(1+exp(theta-x)))
-              P_ij<-as.vector(c(P_ij,output))}
-            for (i in 1:n){
-              x<-a[i]
-              if (y_i[i]==1){
-                output<-(exp(theta-x)/(1+exp(theta-x)))
+            for (i in 1:n){ #a for loop for every single value of a
+              x<-a[i] #this extracts the first value each time and is replaces with each new cycle of the loop
+              output<-(exp(theta-x)/(1+exp(theta-x))) #this takes in our extracted a value and theta value to run the equation
+              P_ij<-as.vector(c(P_ij,output))} #this adds the new value each time so we form a vactor with all of the P_ij values
+            for (i in 1:n){ #a for loop for every single value of a and y_i, this is a little different
+              x<-a[i] #this extracts the first value each time and is replaces with each new cycle of the loop
+              if (y_i[i]==1){ #we set this condition fo the purpose of getting out a P-ij value when the answer is correct
+                output<-(exp(theta-x)/(1+exp(theta-x))) #this is the same as above and sends the value below to be added into the vector
               }
-              if (y_i[i]==0){
-                output<-1-(exp(theta-x)/(1+exp(theta-x)))}
-              PQ_ij<-as.vector(c(PQ_ij,output))
+              if (y_i[i]==0){ #now we are creating a Q_ij value for our wrong answers
+                output<-1-(exp(theta-x)/(1+exp(theta-x)))} #this functionality is the exact same but we just subtract P_ij from ! in order to get our Q_ij
+              PQ_ij<-as.vector(c(PQ_ij,output)) #regardless of whether there is a P_ij and Q_ij value, they will be added here and we can compare this vector to the vector above to know we are right
             }
-            output<-list(P_ij, PQ_ij)
-            names(output)<-c("P_ij", "PQ_ij")
-            return(output)
+            output<-list(P_ij, PQ_ij) #making a list out our outputs so they can both be viewed (and later manipulated)
+            names(output)<-c("P_ij", "PQ_ij") #Naming the values for the ease of the user
+            return(output) #creating out output and ta-da we have our probability vectors!
           }
 )
 
 #code for Likelihood function
-setGeneric(name="likelihood",
-           def=function(raschObj, theta)
+setGeneric(name="likelihood", #doing this again
+           def=function(raschObj, theta) #we have the same inputs above which we will pass into the function we made above
            {standardGeneric("likelihood")}
 )
 
 setMethod(f="likelihood",
-          definition=function(raschObj, theta){
+          definition=function(raschObj, theta){ #mirroring the above for the purpose of the creation of our function
             ourworkingoutputs<-probability(raschObj,theta)
             P_ij<-ourworkingoutputs$P_ij
             PQ_ij<-ourworkingoutputs$PQ_ij
